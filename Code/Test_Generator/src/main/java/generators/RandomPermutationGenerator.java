4b2fc51f32983d90482b0e3b3640cb14f9ac486c
Dgeorgealex <95629219+Dgeorgealex@users.noreply.github.com>
2023-05-28 00:26:06 +0300

    added Candidate Solution generator

--- Code/Test_Generator/src/main/java/generators/RandomPermutationGenerator.java
+++ Code/Test_Generator/src/main/java/generators/RandomPermutationGenerator.java
@@ -1,2 +1,29 @@
-package generators;public class RandomPermutationGenerator {
+package generators;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RandomPermutationGenerator {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < 280; i++) {
+            numbers.add(i);
+        }
+
+        // Shuffle the list to generate a random permutation
+        Collections.shuffle(numbers);
+
+        // Serialize the shuffled list to JSON
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            String json = objectMapper.writeValueAsString(numbers);
+            System.out.println(json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 }
